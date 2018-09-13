fe85f7d9bc0666e0ac92f4555191a1fc7b3d8a48
Mohamed Termoul <mtermoul@gmail.com>
2018-09-12 23:30:42 -0400

    Attempt 2: fix the iPhone scrollbar issue

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,16 @@ export default {
 </script>
 
 <style>
+body, html{
+    overflow-x: hidden;
+}
 #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    overflow-x: hidden;
 }
 #nav {
     padding: 30px;
